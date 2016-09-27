20b3d74733604cf72fe47d6483e3b231c0c9eb36
Austin Mackesy-Buckley <austin-henry.mackesy-buckley@deliveryhero.com>
2016-09-27 20:24:31 +0200

    Keep selector format consistent, rename tests, change origins and destinations, fix class name

--- API_tests/client.py
+++ API_tests/client.py
@@ -7,28 +7,30 @@ import sys
 class DistanceAPIClient(object):
 
     def __init__(self):
-        self.url = self.set_api_url()
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        logging.getLogger("requests").setLevel(logging.ERROR)
+        self.log = logging.getLogger(__name__)
+
+        self.base_url = "https://maps.googleapis.com/maps/api/distancematrix/"
         self.session = requests.Session()
         self.session.headers = {
             "authentication": "key={0}".format(os.environ['API_KEY']),
             "Accept": "application/json"
             }
 
-        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-        logging.getLogger("requests").setLevel(logging.ERROR)
-        self.log = logging.getLogger(__name__)
+    def get_url(self, output):
+        url = self.base_url + output
+        self.log.debug("URL: {0}".format(url))
+        return url
 
-    def set_api_url(self, output="json"):
-        return "https://maps.googleapis.com/maps/api/distancematrix/{0}".format(output)
+    def get_distance(self, data, output="json", expected_code=200):
+        resp = self.session.get(self.get_url(output=output), params=data)
+        self._assert_code(resp, expected_code)
+        return resp
 
-    def assert_code(self, response, expected_code):
+    def _assert_code(self, response, expected_code):
         try:
             assert response.status_code == expected_code
         except AssertionError:
             self.log.info("RESPONSE: {0}".format(response.text.encode("utf-8")))
             raise
-
-    def get_distance(self, data, expected_code=200):
-        resp = self.session.get(self.url, params=data)
-        self.assert_code(resp, expected_code)
-        return resp
--- API_tests/distance_tests.py
+++ API_tests/distance_tests.py
@@ -6,64 +6,92 @@ from client import DistanceAPIClient
 import logging
 
 
-class TestDirectionsAPI(object):
-
+class TestDistanceAPI(object):
+    
     def setup(self):
         self.client = DistanceAPIClient()
         self.log = logging.getLogger(__name__)
 
-    def test_optional_parameters_only(self):
+    def test_multiple_origins_and_destinations_returns_correct_results(self):
+        data = {
+            "origins": "Wellington|Rotorua",
+            "destinations": "Auckland|Wanganui",
+        }
+        resp = self.client.get_distance(data)
+        assert resp.json()["status"] == "OK"
+
+        origins = ["Wellington, New Zealand", "Rotorua, New Zealand"]
+        assert all(origin in resp.json()["origin_addresses"] for origin in origins)
+
+        destinations = ["Auckland, New Zealand", "Whanganui, New Zealand"]
+        assert all(destination in resp.json()["destination_addresses"] for destination in destinations)
+
+        rows = self._get_result_rows(resp)
+        assert len(rows) == 2  # check there are two rows
+
+        self._check_result_elements(rows)
+
+    def test_no_required_parameters_returns_invalid_request(self):
         data = {
             "mode": "bicycling"
         }
         resp = self.client.get_distance(data)
         assert resp.json()["status"] == "INVALID_REQUEST"
 
-    def test_required_parameters_only(self):
+    def test_single_required_parameter_only(self):
         data = {
-            "origins": "Vancouver BC|Seattle",
-            "destinations": "San Francisco|Victoria BC"
-        }
-        resp = self.client.get_distance(data)
-        assert resp.json()["status"] == "OK"
-        origins = ["Vancouver, BC, Canada", "Seattle, WA, USA"]
-        assert all(x in resp.json()["origin_addresses"] for x in origins)
-
-    def test_partial_required_parameters(self):
-        data = {
-            "origins": "Vancouver BC"
+            "origins": "Dublin"
         }
         resp = self.client.get_distance(data)
         assert resp.json()["status"] == "INVALID_REQUEST"
 
-    def test_required_parameters_and_optional_parameters(self):
+    def test_no_output_specified_returns_404(self):
         data = {
-            "origins": "Vancouver BC|Seattle",
-            "destinations": "San Francisco|Victoria BC",
+            "origins": "Limerick",
+            "destinations": "Galway",
+            "mode": "bicycling"
+        }
+        self.client.get_distance(data, output="", expected_code=404)
+
+    def test_results_in_language_specified_as_optional_parameter(self):
+        data = {
+            "origins": "Limerick",
+            "destinations": "Galway",
             "language": "fr"
         }
         resp = self.client.get_distance(data)
         assert resp.json()["status"] == "OK"
-        french = "San Francisco, Californie, États-Unis"
+
+        french = "Galway, Irlande"
         assert french.decode("utf-8") in resp.json()["destination_addresses"]
 
-    def test_no_supported_language(self):
+    def test_results_in_domain_language_when_no_supported_language_parameter(self):
         data = {
-            "origins": "Vancouver BC|Seattle",
-            "destinations": "San Francisco|Victoria BC",
+            "origins": "Limerick",
+            "destinations": "Galway",
             "language": "xx"
         }
         resp = self.client.get_distance(data)
         assert resp.json()["status"] == "OK"
-        english = "San Francisco, CA, USA"
-        assert english in resp.json()["destination_addresses"]
 
-    def test_no_output_specified(self):
-        self.client.url = self.client.set_api_url(output=None)
-        data = {
-            "origins": "Vancouver BC|Seattle",
-            "destinations": "San Francisco|Victoria BC",
-            "mode": "bicycling"
-        }
-        resp = self.client.get_distance(data, 404)
-        self.client.url = self.client.set_api_url()
+        english = "Galway, Ireland"
+        assert english in resp.json()["destination_addresses"]
+        
+    def _get_result_rows(self, resp):
+        return resp.json()["rows"]
+
+    def _check_result_elements(self, rows):
+        for row in rows:
+            elements = row["elements"]
+            assert len(elements) == 2  # check there are two elements within each row 
+            self._check_element_details(elements)
+
+    def _check_element_details(self, elements):
+        for element in elements:
+            assert element["status"] == "OK"  # check the status for each element is OK
+
+            duration_value = element["duration"]["value"]
+            assert isinstance(duration_value, int) and duration_value > 0  # check duration value is an integer and above zero
+
+            distance_value = element["distance"]["value"]
+            assert isinstance(distance_value, int) and duration_value > 0  # check distance value is an integer and above zero
